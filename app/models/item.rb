927d044c5f26a681ef6de140c55744fe355163e7
Sudipto Ghosh <sudipto@jhatkaa.org>
2022-09-27 16:34:23 +0530

    Setup for Production and Add WebP Variation (#1)
    * add webp variation
    
    * add credentials for production
    
    * add host to production config

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,19 +2,23 @@ class Item < ApplicationRecord
   # has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
   has_attached_file :file, styles: lambda { |attachment|
     if attachment.instance.image?
-      { display: '800x800>', small: '400x400>', thumbnail: '100x100>' }
+      { display: '800x800>',
+        small: '400x400>',
+        thumbnail: '100x100>',
+        display_webp: ['800x800>', :webp],
+        small_webp: ['400x400>', :webp],
+        thumbnail_webp: ['100x100>', :webp] }
     else
       {}
     end
   }
-
   # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   do_not_validate_attachment_file_type :file
   # Tagging
   acts_as_taggable
 
   validates_uniqueness_of :file_fingerprint,
-    message: 'Error: This file has already been uploaded - please try searching for it by filename or by tag.'
+                          message: 'Error: This file has already been uploaded - please try searching for it by filename or by tag.'
   # TODO: link to previous upload (find by fingerprint)
 
   # Check to see if the file is an image before generating thumbnails etc
